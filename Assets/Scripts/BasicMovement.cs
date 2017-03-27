8bd7d3f37cf6408ba3dc9361ec6048b03529db63
ttanatb <ttanatb@gmail.com>
2017-03-27 17:50:53 -0400

    Initial commit

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    const float SPEED = 30;
+    const float ROT_SPEED = 60;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = transform.position;
+        if (Input.GetKey(KeyCode.W))
+            newPos.y = newPos.y + SPEED * Time.deltaTime;
+        else if (Input.GetKey(KeyCode.S))
+            newPos.y = newPos.y - SPEED * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.D))
+            newPos.x = newPos.x + SPEED * Time.deltaTime;
+        else if (Input.GetKey(KeyCode.A))
+            newPos.x = newPos.x - SPEED * Time.deltaTime;
+
+        if (Input.GetMouseButton(0))
+            newPos.z = newPos.z + SPEED * Time.deltaTime;
+        else if (Input.GetMouseButton(1))
+            newPos.z = newPos.z - SPEED * Time.deltaTime;
+
+        transform.position = newPos;
+
+        Vector3 mousePos = Input.mousePosition;
+        if (mousePos.x < 100)
+            transform.Rotate(-Vector3.up * ROT_SPEED * Time.deltaTime);
+        else if (mousePos.x > Screen.width - 100)
+            transform.Rotate(Vector3.up* ROT_SPEED * Time.deltaTime);
+
+
+        if (mousePos.y < 100)
+            transform.Rotate(-Vector3.left * ROT_SPEED * Time.deltaTime);
+        else if (mousePos.y > Screen.height - 100)
+            transform.Rotate(Vector3.left * ROT_SPEED * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SongChooser.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+public class SongChooser : MonoBehaviour
+{
+    public int BPM = 130;
+    private AudioSource beatAudioSource;
+    private AudioSource tuneAudioSource;
+
+    public AudioClip[] audioClips;
+
+    private float timer = 0f;
+    private int counter = 0;
+    private float crochet = 60f;
+
+    private Transform cameraTransform;
+    private Vector3 prevEuAngle;
+    private float prevY;
+
+    // Use this for initialization
+    void Start()
+    {
+        tuneAudioSource = transform.FindChild("Tune").GetComponent<AudioSource>();
+        beatAudioSource = transform.FindChild("Beat").GetComponent<AudioSource>();
+
+        cameraTransform = Camera.main.transform;
+        prevEuAngle = cameraTransform.rotation.eulerAngles;
+        prevY = cameraTransform.position.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer += Time.fixedDeltaTime;
+
+        if (timer > crochet / BPM)
+        {
+            timer -= crochet / BPM;
+
+            if (CheckIfDancing())
+            {
+                if (counter % 4 == 0)
+                {
+                    if (beatAudioSource.isPlaying)
+                        beatAudioSource.Stop();
+
+                    beatAudioSource.Play();
+                }
+
+                if (counter > 3)
+                {
+                    if (tuneAudioSource.isPlaying)
+                        tuneAudioSource.Stop();
+
+                    tuneAudioSource.clip = audioClips[Random.Range(0, audioClips.Length)];
+                    tuneAudioSource.Play();
+                }
+                counter++;
+            }
+            else
+            {
+                beatAudioSource.Stop();
+                tuneAudioSource.Stop();
+            }
+        }
+
+        prevEuAngle = cameraTransform.eulerAngles;
+        prevY = cameraTransform.position.y;
+    }
+
+    private bool CheckIfDancing()
+    {
+        if ((prevEuAngle - cameraTransform.eulerAngles).sqrMagnitude > 15f || Mathf.Abs(prevY - cameraTransform.position.y) > 0.3f)
+        {
+            return true;
+        }
+        else
+        {
+            counter = 0;
+            return false;
+        }
+    }
+}
